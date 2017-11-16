ce97916bbbb147f25e075cdb77f059f09dd4e24c
kallinski <pascal.waechter94@web.de>
2017-11-16 20:53:12 +0100

    few texture changes and refactorings

--- main.py
+++ main.py
@@ -19,27 +19,28 @@ DIRT  = 0
 GRASS = 1
 WATER = 2
 COAL  = 3
+DK_GRASS = 4
+SAND = 6
+TREE = 7
 
 #a dictionary linking resources to textures
 textures =   {
                 DIRT   : pygame.image.load('dirt.png'),
                 GRASS  : pygame.image.load('grass.png'),
+                DK_GRASS  : pygame.image.load('dark_grass.png'),
                 WATER  : pygame.image.load('water.png'),
                 COAL   : pygame.image.load('coal.png'),
-            }
-
-textures1 = {
-                DIRT   : BROWN,
-                GRASS  : GREEN,
-                WATER  : BLUE,
-                COAL   : BLACK,
+                SAND   : pygame.image.load('sand.png'),
+                TREE   : pygame.image.load('bausch.png'),
             }
 
 inventory =   {
                 DIRT   : 0,
                 GRASS  : 0,
+                DK_GRASS: 0,
                 WATER  : 0,
-                COAL   : 0
+                COAL   : 0,
+                SAND: 0
             }
 
 #useful game dimensions
@@ -49,136 +50,176 @@ SCREENHEIGHT = 1080 - TILESIZE*2
 MAPWIDTH  = int(SCREENWIDTH / TILESIZE)
 MAPHEIGHT = int(SCREENHEIGHT / TILESIZE)
 HEIGHT_OFF = 2*TILESIZE
+DISPLAYSURF = None
+INVFONT = None
+CLOCK = None
 
-#the player image
+resources = [DIRT,GRASS,WATER,COAL, SAND, DK_GRASS]
 PLAYER = pygame.image.load('player.png')
-#the position of the player [x,y]
-playerPos = [0,0]
-
-#a list of resources
-resources = [DIRT,GRASS,WATER,COAL]
-#use list comprehension to create our tilemap
 tilemap = [ [DIRT for w in range(MAPWIDTH)] for h in range(MAPHEIGHT) ]
+treemap = [ [None for w in range(MAPWIDTH)] for h in range(MAPHEIGHT) ]
 
-#set up the display
-pygame.init()
-modes = pygame.display.list_modes(16)
-DISPLAYSURF = pygame.display.set_mode((MAPWIDTH*TILESIZE, MAPHEIGHT*TILESIZE + HEIGHT_OFF), FULLSCREEN)
+class Player():
+    def __init__(self):
+        self.xPos = 0
+        self.yPos = 0
 
+    def move(self, x, y):
+        self.xPos += x
+        self.yPos += y
 
-#add a font for our inventory
-INVFONT = pygame.font.Font('Font.ttf', 18)
+    def update(self):
+        DISPLAYSURF.blit(PLAYER, (int(MAPWIDTH / 2) * TILESIZE, int(MAPHEIGHT / 2) * TILESIZE))
 
-mapClock = pygame.time.Clock()
-displayClock = pygame.time.Clock()
+class Inventory():
+    def update(self):
+        # display the inventory, starting 10 pixels in
+        placePosition = 10
+        for item in resources:
+            # add the image
+            DISPLAYSURF.blit(textures[item], (placePosition, MAPHEIGHT * TILESIZE + 20))
+            placePosition += 30
+            # add the text showing the amount in the inventory
+            textObj = INVFONT.render(str(inventory[item]), True, WHITE, BLACK)
+            DISPLAYSURF.blit(textObj, (placePosition, MAPHEIGHT * TILESIZE + 20))
+            placePosition += 50
 
-def updateMap(xPos, yPos):
-    # loop through each row
-    for row in range(MAPHEIGHT):
-        # loop through each column in the row
-        for column in range(MAPWIDTH):
-            kx = (xPos + column) / MAPWIDTH - 0.5
-            ky = (yPos + row) / MAPHEIGHT - 0.5
-            value = 0.9 * noise(0.5 * kx, 0.5 * ky) \
+class Map():
+    def update(self, xPos, yPos):
+        # loop through each row
+        for row in range(MAPHEIGHT):
+            # loop through each column in the row
+            for column in range(MAPWIDTH):
+
+                kx = (xPos + column) / MAPWIDTH - 0.5
+                ky = (yPos + row) / MAPHEIGHT - 0.5
+
+                #elevation
+                e = 0.5 * noise(0.5 * kx, 0.5 * ky) \
                     + 0.4 * noise(2 * kx, 2 * ky) \
-                    + 0.15 * noise(2 * kx, 2 * ky)
+                    + 0.1 * noise(2 * kx, 2 * ky)
 
-            if value > 0.65 and value <= 0.7:
-                tile = DIRT
-            elif value > 0.7 and value <= 0.9:
-                tile = GRASS
-            elif value > 0.9:
-                tile = COAL
-            else:
-                tile = WATER
+                v = 0.6 * noise(0.5 * kx, 0.5 * ky) \
+                    + 0.3 * noise(2 * kx, 2 * ky) \
+                    + 0.2 * noise(2 * kx, 2 * ky)
 
-            tilemap[row][column] = tile
+                tile = self.biome(e, v)
 
-            DISPLAYSURF.blit(textures[tilemap[row][column]], (column * TILESIZE, row * TILESIZE))
+                tilemap[row][column] = tile
 
-#loop through each row
-for rw in range(MAPHEIGHT):
-    #loop through each column in that row
-    for cl in range(MAPWIDTH):
-        kx = (cl) / MAPWIDTH - 0.5
-        ky = (rw) / MAPHEIGHT - 0.5
-        value = 0.75 * noise(0.5 * kx, 0.5 * ky) \
-                + 0.5 * noise(2 * kx, 2 * ky) \
-                + 0.25 * noise(3 * kx, 3 * ky)
+                DISPLAYSURF.blit(textures[tile], (column * TILESIZE, row * TILESIZE))
 
-        if value > 0.65 and value <= 0.7:
-            tile = DIRT
-        elif value > 0.7 and value <= 0.9:
-            tile = GRASS
-        elif value > 0.9:
-            tile = COAL
-        else:
-            tile = WATER
-        #set the position in the tilemap to the randomly chosen tile
-    tilemap[rw][cl] = tile
+                plants = 1 * noise(4 * kx, 2 * ky)
+                if plants > 0.7 and plants < 0.8 and tile != WATER and tile != COAL:
+                    DISPLAYSURF.blit(textures[TREE], (column * TILESIZE, row * TILESIZE))
+                    treemap[row][column] = TREE
+                else:
+                    treemap[row][column] = None
 
+    def biome(self, e, v):
+        if e < 0.4: return WATER
+        if e < 0.45: return SAND
 
-while True:
+        if e < 0.6:
+            if v < 0.5: return DK_GRASS
+            return GRASS
+        if e < 0.7:
+            if v < 0.2: return SAND
+            return DIRT
+        if e < 0.8:
+            if v < 0.8: return DK_GRASS
+            return COAL
+        return COAL
 
-    #get all the user events
-    for event in pygame.event.get():
-        #if the user wants to quit
-        if event.type == QUIT:
-            #and the game and close the window
+class App():
+    def __init__(self):
+        global DISPLAYSURF, INVFONT, CLOCK
+        # set up the display
+        pygame.init()
+        modes = pygame.display.list_modes(16)
+        DISPLAYSURF = pygame.display.set_mode((MAPWIDTH * TILESIZE, MAPHEIGHT * TILESIZE + HEIGHT_OFF), FULLSCREEN)
+        # add a font for our inventory
+        INVFONT = pygame.font.Font('Font.ttf', 18)
+        CLOCK = pygame.time.Clock()
+        self.map = Map()
+        self.inventory = Inventory()
+        self.player = Player()
+        self.loop()
+
+    def OnEvent(self, event):
+            # if the user wants to quit
+            if event.type == QUIT:
+                # and the game and close the window
+                pygame.quit()
+                sys.exit()
+            # if a key is pressed
+            elif event.type == KEYDOWN:
+                self.OnKeydownEvent(event.key)
+
+    def OnKeysPressed(self, keys):
+        # if the right arrow is pressed
+        if (keys[K_RIGHT] or keys[K_d]) \
+                and tilemap[int(MAPHEIGHT / 2)][int(MAPWIDTH / 2) + 1] != WATER \
+                and treemap[int(MAPHEIGHT / 2)][int(MAPWIDTH / 2) + 1] != TREE:
+            # change the player's x position
+            self.player.move(+1, 0)
+        if (keys[K_LEFT] or keys[K_a]) \
+                and tilemap[int(MAPHEIGHT / 2)][int(MAPWIDTH / 2) - 1] != WATER \
+                and treemap[int(MAPHEIGHT / 2)][int(MAPWIDTH / 2) - 1] != TREE:
+            # change the player's x position
+            self.player.move(-1, 0)
+        if (keys[K_UP] or keys[K_w]) \
+                and tilemap[int(MAPHEIGHT / 2) - 1][int(MAPWIDTH / 2)] != WATER \
+                and treemap[int(MAPHEIGHT / 2) - 1][int(MAPWIDTH / 2)] != TREE:
+            # change the player's x position
+            self.player.move(0, -1)
+        if (keys[K_DOWN] or keys[K_s]) \
+                and tilemap[int(MAPHEIGHT / 2) + 1][int(MAPWIDTH / 2)] != WATER \
+                and treemap[int(MAPHEIGHT / 2) + 1][int(MAPWIDTH / 2)] != TREE:
+            # change the player's x position
+            self.player.move(0, +1)
+
+    def OnKeydownEvent(self, key):
+        # placing dirt
+        if (key == K_1):
+            # get the tile to swap with the dirt
+            currentTile = tilemap[(int(MAPHEIGHT / 2))][(int(MAPWIDTH / 2))]
+            # if we have dirt in our inventory
+            if inventory[DIRT] > 0:
+                # remove one dirt and place it
+                inventory[DIRT] -= 1
+                # swap the item that was there before
+                inventory[currentTile] += 1
+        elif key == K_SPACE:
+            # what resource is the player standing on?
+            currentTile = tilemap[(int(MAPHEIGHT / 2))][(int(MAPWIDTH / 2))]
+            # player now has 1 more of this resource
+            inventory[currentTile] += 1
+            # the player is now standing on dirt
+            tilemap[(int(MAPHEIGHT / 2))][(int(MAPWIDTH / 2))] = DIRT
+        elif key == K_ESCAPE:
             pygame.quit()
             sys.exit()
-        #if a key is pressed
-        elif event.type == KEYDOWN:
-            #placing dirt
-            if (event.key == K_1):
-                #get the tile to swap with the dirt
-                currentTile = tilemap[playerPos[1]][playerPos[0]]
-                #if we have dirt in our inventory
-                if inventory[DIRT] > 0:
-                    #remove one dirt and place it
-                    inventory[DIRT] -= 1
-                    tilemap[playerPos[1]][playerPos[0]] = DIRT
-                    #swap the item that was there before
-                    inventory[currentTile] += 1
-            elif event.key == K_SPACE:
-                # what resource is the player standing on?
-                currentTile = tilemap[playerPos[1]][playerPos[0]]
-                # player now has 1 more of this resource
-                inventory[currentTile] += 1
-                # the player is now standing on dirt
-                tilemap[playerPos[1]][playerPos[0]] = DIRT
 
+    def loop(self):
 
-    keys = pygame.key.get_pressed()
-    # if the right arrow is pressed
-    if (keys[K_RIGHT] or keys[K_d]):
-        # change the player's x position
-        playerPos[0] += 1
-    if (keys[K_LEFT] or keys[K_a]):
-        # change the player's x position
-        playerPos[0] -= 1
-    if (keys[K_UP] or keys[K_w]):
-        # change the player's x position
-        playerPos[1] -= 1
-    if (keys[K_DOWN] or keys[K_s]):
-        # change the player's x position
-        playerPos[1] += 1
+        #initial map
+        self.map.update(0, 0)
 
-    #display the inventory, starting 10 pixels in
-    placePosition = 10
-    for item in resources:
-        #add the image
-        DISPLAYSURF.blit(textures[item],(placePosition,MAPHEIGHT*TILESIZE+20))
-        placePosition += 30
-        #add the text showing the amount in the inventory
-        textObj = INVFONT.render(str(inventory[item]), True, WHITE, BLACK)
-        DISPLAYSURF.blit(textObj,(placePosition,MAPHEIGHT*TILESIZE+20))
-        placePosition += 50
+        while True:
+            for event in pygame.event.get():
+                self.OnEvent(event)
 
-    updateMap(playerPos[0], playerPos[1])
-    DISPLAYSURF.blit(PLAYER, (int(MAPWIDTH / 2)*TILESIZE, int(MAPHEIGHT / 2)*TILESIZE))
+            self.OnKeysPressed(pygame.key.get_pressed())
 
-    # update the display
-    pygame.display.update()
+            self.inventory.update()
+            self.map.update(self.player.xPos, self.player.yPos)
+            self.player.update()
 
-    mapClock.tick(60)
+            # update the display
+            pygame.display.update()
+
+            CLOCK.tick(60)
+
+if __name__ == '__main__':
+    app = App()
