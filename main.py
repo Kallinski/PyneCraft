c0cb9527f0a8a8db3343aa5d2da6ba29ecc13422
kallinski <pascal.waechter94@web.de>
2017-11-19 00:25:08 +0100

    implemented chunking and saving/loading generated chunks

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pygame, sys, random, _thread, math
-import numpy as np
+import pygame, sys, random, math, sqlite3, json
 from pygame.locals import *
 from opensimplex import OpenSimplex
 
@@ -8,10 +7,7 @@ def noise(nx, ny):
     # Rescale from -1.0:+1.0 to 0.0:1.0
     return gen.noise2d(nx, ny) / 2.0 + 0.5
 
-def angle_between(p1, p2):
-    ang1 = np.arctan2(*p1[::-1])
-    ang2 = np.arctan2(*p2[::-1])
-    return np.rad2deg((ang1 - ang2) % (2 * np.pi))
+DEBUG = False
 
 #constants representing colours
 BLACK = (0,   0,   0  )
@@ -43,11 +39,11 @@ inventory = {
 textures =  {
             DIRT: pygame.image.load('dirt1.png'),
             GRASS: pygame.image.load('grass1.png'),
-            DGRASS: pygame.image.load('dgrass1.png'),
+            DGRASS: pygame.image.load('grass3.png'),
             WATER: pygame.image.load('water1.png'),
             STONE: pygame.image.load('stone1.png'),
             SAND: pygame.image.load('sand1.png'),
-            TREE: pygame.image.load('tree1.png'),
+            TREE: pygame.image.load('tree2.png'),
             }
 
 #useful game dimensions
@@ -60,6 +56,8 @@ HEIGHT_OFF = 2*TILESIZE
 DISPLAYSURF = None
 INVFONT = None
 CLOCK = None
+TPS = 64 #tiles per chunk
+MAX_CHUNKS = 4 + int(MAPWIDTH / TPS) * int(MAPHEIGHT / TPS)
 
 FSCREEN = False
 
@@ -69,6 +67,9 @@ PLAYER = PLAYER_ORIG
 SELECTION = pygame.image.load('selection.png')
 tilemap = [ [DIRT for w in range(MAPWIDTH)] for h in range(MAPHEIGHT) ]
 treemap = [ [None for w in range(MAPWIDTH)] for h in range(MAPHEIGHT) ]
+
+chunksGround = {}
+chunksObjects = {}
 
 class Player():
     def __init__(self):
@@ -134,29 +135,119 @@ class Inventory():
             placePosition += 50
 
 class Map():
+    def __init__(self):
+        global gen, tilemap
+        # initial map
+        self.update(0, 0)
+        startTile = tilemap[int(MAPHEIGHT / 2)][int(MAPWIDTH / 2)]
+        while startTile == WATER:
+            gen = OpenSimplex(seed=random.randint(0, 99999999999))
+            self.update(0, 0)
+            startTile = tilemap[int(MAPHEIGHT / 2)][int(MAPWIDTH / 2)]
+
+    def worldCoordinatesToChunk(self, x, y):
+        xChunk = int(x / TPS)
+        yChunk = int(y / TPS)
+
+        if x < 0: xChunk -= 1
+        if y < 0: yChunk -= 1
+        return xChunk, yChunk
+
+    def generateChunk(self, x, y):
+        id = str(x)+","+str(y)
+        App.c.execute('''SELECT id FROM map WHERE id=?''', (id,))
+
+        if App.c.fetchone() == None:
+
+            ground = [[DIRT for w in range(TPS)] for h in range(TPS)]
+            objects = [ [None for w in range(TPS)] for h in range(TPS) ]
+            for row in range(0,TPS):
+                for column in range(0,TPS):
+
+                    kx = (x*TPS + column) / MAPWIDTH# - 0.5
+                    ky = (y*TPS + row) / MAPHEIGHT# - 0.5
+
+                    e, v = self.noiseParameter(kx, ky)
+
+                    tile = self.biome(e, v)
+
+                    ground[row][column] = tile
+
+                    plants = 0.25 * noise(4 * kx, 2 * ky)
+                    if plants > 0.1 and plants < 0.11 and tile != WATER and tile != STONE:
+                        objects[row][column] = TREE
+                    else:
+                        objects[row][column] = None
+            App.c.execute('''INSERT INTO map(id, x, y, ground, objects) VALUES(?,?,?,?,?)''', (str(x)+","+str(y), x, y, json.dumps(ground), json.dumps(objects)))
+            App.conn.commit()
+
+            return ground, objects
+
+    def loadChunk(self, x, y):
+        id = str(x) + "," + str(y)
+        App.c.execute('''SELECT id FROM map WHERE id=?''', (id,))
+        res = App.c.fetchone()
+
+        if res != None:
+
+            App.c.execute('''SELECT ground FROM map WHERE id=?''', (id,))
+            ground = json.loads(App.c.fetchone()[0])
+
+            App.c.execute('''SELECT objects FROM map WHERE id=?''', (id,))
+            objects = json.loads(App.c.fetchone()[0])
+
+        else:
+
+            ground, objects = self.generateChunk(x, y)
+
+        chunksGround.update({str(x)+","+str(y): ground})
+        chunksObjects.update({str(x)+","+str(y): objects})
+
+
+        return ground, objects
+
+    def unloadChunk(self, x, y):
+        chunksGround.pop(str(x)+","+str(y), None)
+        chunksObjects.pop(str(x)+","+str(y), None)
+
     def update(self, xPos, yPos):
-        # loop through each row
+
+        neededChunks = []
+
         for row in range(MAPHEIGHT):
-            # loop through each column in the row
             for column in range(MAPWIDTH):
+                cx, cy = self.worldCoordinatesToChunk(xPos + column, yPos + row)
+                neededChunks.append(str(cx)+","+str(cy))
+                if chunksGround.get(str(cx)+","+str(cy)) != None and chunksObjects.get(str(cx)+","+str(cy)) != None:
+                    ground = chunksGround.get(str(cx)+","+str(cy))
+                    objects = chunksObjects.get(str(cx)+","+str(cy))
+                else:
+                    ground, objects = self.loadChunk(cx, cy)
 
-                kx = (xPos + column) / MAPWIDTH# - 0.5
-                ky = (yPos + row) / MAPHEIGHT# - 0.5
+                chunkX = (xPos+column) % TPS
+                chunkY = (yPos+row) % TPS
 
-                e, v = self.noiseParameter(kx, ky)
-
-                tile = self.biome(e, v)
+                tile = ground[chunkY][chunkX]
+                object = objects[chunkY][chunkX]
 
                 tilemap[row][column] = tile
+                treemap[row][column] = object
 
                 DISPLAYSURF.blit(textures[tile], (column * TILESIZE, row * TILESIZE))
+                if object != None:
+                    DISPLAYSURF.blit(textures[object], (column * TILESIZE, row * TILESIZE))
 
-                plants = 0.25 * noise(4 * kx, 2 * ky)
-                if plants > 0.1 and plants < 0.11 and tile != WATER and tile != STONE:
-                    DISPLAYSURF.blit(textures[TREE], (column * TILESIZE, row * TILESIZE))
-                    treemap[row][column] = TREE
-                else:
-                    treemap[row][column] = None
+                if DEBUG == True:
+                    textObj = INVFONT.render(str(xPos+column)+","+str(yPos+row), True, WHITE, BLACK)
+                    DISPLAYSURF.blit(textObj, (column * TILESIZE, row * TILESIZE))
+
+                    textObj = INVFONT.render(str(chunkX) + "," + str(chunkY), True, BLACK, WHITE)
+                    DISPLAYSURF.blit(textObj, (column * TILESIZE, row * TILESIZE+10))
+
+                    textObj = INVFONT.render(str(cx) + "," + str(cy), True, BLACK, GREEN)
+                    DISPLAYSURF.blit(textObj, (column * TILESIZE, row * TILESIZE + 20))
+
+
 
     def biome(self, e, v):
         if e < 0.4: return WATER
@@ -193,8 +284,17 @@ class App():
         modes = pygame.display.list_modes(16)
         DISPLAYSURF = pygame.display.set_mode((MAPWIDTH * TILESIZE, MAPHEIGHT * TILESIZE + HEIGHT_OFF))
         # add a font for our inventory
-        INVFONT = pygame.font.Font('Font.ttf', 18)
+        INVFONT = pygame.font.Font('Font.ttf', 8)
         CLOCK = pygame.time.Clock()
+
+        App.conn = sqlite3.connect('map.db')
+        App.c = self.conn.cursor()
+        App.c.execute('''SELECT name FROM sqlite_master WHERE type='table' AND name='map';''')
+        res = App.c.fetchall()
+        if ('map',) in res:
+            App.c.execute('''DROP TABLE map''')
+        App.c.execute('''CREATE TABLE map (id string, x int, y int, ground blob, objects blob, PRIMARY KEY (id))''')
+
         self.map = Map()
         self.inventory = Inventory()
         self.player = Player()
@@ -271,20 +371,18 @@ class App():
             else:
                 DISPLAYSURF = pygame.display.set_mode((MAPWIDTH * TILESIZE, MAPHEIGHT * TILESIZE + HEIGHT_OFF))
 
+        elif key == K_F12:
+            global DEBUG
+            DEBUG = not DEBUG
+
     def OnMouseMovement(self, x, y):
         angle = self.player.rotateTo(x, y)
         self.player.selectNearestTile(x, y)
 
     def loop(self):
 
-        global gen, tilemap
-        #initial map
-        self.map.update(0, 0)
-        startTile = tilemap[int(MAPHEIGHT / 2)][int(MAPWIDTH / 2)]
-        while startTile == WATER:
-            gen = OpenSimplex(seed=random.randint(0, 99999999999))
-            self.map.update(0, 0)
-            startTile = tilemap[int(MAPHEIGHT / 2)][int(MAPWIDTH / 2)]
+        self.map.generateChunk(0, 0)
+        self.map.loadChunk(0, 0)
 
         while True:
             for event in pygame.event.get():
